4a0f07238738499f795eb29de41279a38257e2d1
puliya2002 <141041873+puliya2002@users.noreply.github.com>
2024-12-10 16:20:20 +0530

    event card

--- Frontend-React/src/Components/EventCard.jsx
+++ Frontend-React/src/Components/EventCard.jsx
@@ -5,17 +5,25 @@ function EventCard({ image, title, price, location }) {
   return (
     <div className="card bg-base-100 shadow-xl">
       <figure>
-        <img src={image} alt={title} className="w-full h-48 object-cover" />
+        <div className="flex flex-col  absolute top-4 left-4 w-auto  border-white border-[3px]">
+          <div className=" h-10 bg-white text-2xl text-primary font-bold  flex items-center justify-center w-full">
+            19
+          </div>
+          <div className=" h-10 bg-secondary text-white flex items-center justify-center min-w-28 px-2">
+            March
+          </div>
+        </div>
+        <img src={image} alt={title} className="w-full h-58 object-cover" />
       </figure>
       <div className="card-body">
         <h2 className="card-title text-lg font-semibold">{title}</h2>
-        <p className="text-primary text-xl font-medium">RS {price}</p>
+        <p className="text-primary text-md font-bold">RS {price}</p>
         <div className="flex items-center gap-2 text-sm text-gray-600">
           <FaLocationDot className="text-red-500" />
           <p>{location}</p>
         </div>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn rounded-full border-gray-200 h-5 w-32 text-primary">Buy Now</button>
         </div>
       </div>
     </div>
--- Frontend-React/src/pages/Event.jsx
+++ Frontend-React/src/pages/Event.jsx
@@ -7,7 +7,7 @@ function Event() {
       <div className="container mx-auto section_px items-center flex flex-col">
         <h1 className="text-center pb-10">All events</h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 c">
+        <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 p-2 c">
           <EventCard
             image="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
             title="Musical Show"
